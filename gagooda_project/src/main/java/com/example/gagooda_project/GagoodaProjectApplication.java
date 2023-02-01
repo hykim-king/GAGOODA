75cc0b233c66eab3518b76174ecd493d3af22750
hamin081234 <hamin081234@gmail.com>
2023-02-01 23:38:36 +0900

    프로젝트 생성

---
+++ gagooda_project/src/main/java/com/example/gagooda_project/GagoodaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gagooda_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagoodaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GagoodaProjectApplication.class, args);
+	}
+
+}
---
+++ gagooda_project/src/test/java/com/example/gagooda_project/GagoodaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gagooda_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagoodaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
