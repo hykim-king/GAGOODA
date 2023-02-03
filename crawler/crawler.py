8dbdc564dd2583564d58261a024707b25cb695a7
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-02-03 21:53:43 +0900

    크롤러

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -132,15 +132,15 @@ def img_save(img_path, content):
 class Crawler:
     def __init__(self):
         # 크롤링을 시작 할 페이지
-        self.start_url = "https://www.iloom.com/product/item.do?categoryNo=10"
+        self.start_url = "https://www.iloom.com/product/item.do?categoryNo=13"
         self.visited_list = set()
         self.to_visit = set()
         # 상품과 연결될 카테고리
-        self.category = ['침실', '수납장']
+        self.category = ['거실', '소파']
         # csv 파일이 저장될 경로
-        self.csv_path = "C:/Users/hamin/Desktop/data/chm/csv/"
+        self.csv_path = "C:/Users/oyunm/Desktop/crawling_chy/"
         # 이미지 파일이 저장될 경로
-        self.img_path = 'C:/Users/hamin/Desktop/data/chm/img/'
+        self.img_path = 'C:/Users/oyunm/Pictures/iloom/'
 
     def parse_html(self, url):
         soup = None
