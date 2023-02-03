1cde56af7d15816659a67901c609ca424dc310ba
yangrangrang <gkssk2309@gmail.com>
2023-02-03 09:46:07 +0900

    카테고리, 카테고리연결 DTO

---
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/CategoryConnDto.java
@@ -0,0 +1,12 @@
+package com.example.gagooda_project.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryConnDto {
+    private int categoryConnId;
+
+    private int categoryId;
+
+    private int productId;
+}
---
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/CategoryDto.java
@@ -0,0 +1,15 @@
+package com.example.gagooda_project.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDto {
+    private int categoryId;
+
+    private int higherLevelId;
+
+    private String name;
+
+    private String lvl;
+
+}
