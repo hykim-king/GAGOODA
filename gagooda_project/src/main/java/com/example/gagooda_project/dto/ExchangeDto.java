f0425dd09376ef7333361c16b470c19edf6c72f0
유한비 <119904360+hanbbi@users.noreply.github.com>
2023-02-03 16:37:32 +0900

    dto 수정

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/ExchangeDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/ExchangeDto.java
@@ -6,16 +6,16 @@ import java.util.Date;
 
 @Data
 public class ExchangeDto {
-    private int exchangeId;
-    private int orderDetailId;
-    private int userId;
-    private String productId;
-    private String exMst;
-    private String exDet;
-    private String rfrMst;
-    private String rfrDet;
-    private String exchaReason;
-    private int exchaCount;
-    private Date exchaDate;
-    private int exchaState;
+    private int exchangeId;  // 교환 ID, PK, NN
+    private int userId;  // 사용자 ID, FK, NN, N:1
+    private int orderDetailId;  // 주문 상세 ID, FK, NN, 1:1
+    private String productId;  // 상품 ID, FK, NN, 1:1
+    private String exMst;  // 교환처리상태_MST, FK, NN, N:1
+    private String exDet;  // 교환처리상태_DET, FK, NN, N:1
+    private String rfrMst;  // 환불/교환 사유_MST, FK, NN, N:1
+    private String rfrDet;  // 환불/교환 사유_DET, FK, NN, N:1
+    private String exchaReason;  // 교환 사유, NN
+    private int exchaCount;  // 교환 수량, NN
+    private Date exchaDate;  // 교환일, NN
+    private boolean exchaState;  // 교환 상태, NN
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
@@ -6,16 +6,16 @@ import java.util.Date;
 
 @Data
 public class RefundDto {
-    private int refundId;  // 환불 ID
-    private int userId;  // 사용자 ID
-    private int orderDetailId;  // 주문 상세 ID
-    private String rfMst;  // 환불처리상태_MST
-    private String rfDet;  // 환불처리상태_DET
-    private String rfrMst;  // 환불/교환 사유_MST
-    private String rfrDet;  // 환불/교환 사유_DET
-    private String reason;  // 환불 사유
-    private int count;  // 환불 수량
-    private Date date;  // 환불 일자
-    private int status;  // 환불 상태
-    private String imgCode;  // 이미지 코드
+    private int refundId;  // 환불 ID, PK, NN
+    private int userId;  // 사용자 ID, FK, NN, N:1
+    private int orderDetailId;  // 주문 상세 ID, FK, NN, 1:1
+    private String rfMst;  // 환불처리상태_MST, FK, NN, N:1
+    private String rfDet;  // 환불처리상태_DET, FK, NN, N:1
+    private String rfrMst;  // 환불/교환 사유_MST, FK, NN, N:1
+    private String rfrDet;  // 환불/교환 사유_DET, FK, NN, N:1
+    private String reason;  // 환불 사유, NN
+    private int count;  // 환불 수량, NN
+    private Date date;  // 환불 일자, NN
+    private boolean status;  // 환불 상태, NN
+    private String imgCode;  // 이미지 코드, NULL
 }
