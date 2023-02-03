3f212d6aff1f76100aefa23565a16da2257ad07b
유한비 <119904360+hanbbi@users.noreply.github.com>
2023-02-03 16:54:34 +0900

    dto 주석 수정

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/ExchangeDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/ExchangeDto.java
@@ -7,13 +7,13 @@ import java.util.Date;
 @Data
 public class ExchangeDto {
     private int exchangeId;  // 교환 ID, PK, NN
-    private int userId;  // 사용자 ID, FK, NN, N:1
-    private int orderDetailId;  // 주문 상세 ID, FK, NN, 1:1
-    private String productId;  // 상품 ID, FK, NN, 1:1
-    private String exMst;  // 교환처리상태_MST, FK, NN, N:1
-    private String exDet;  // 교환처리상태_DET, FK, NN, N:1
-    private String rfrMst;  // 환불/교환 사유_MST, FK, NN, N:1
-    private String rfrDet;  // 환불/교환 사유_DET, FK, NN, N:1
+    private int userId;  // 사용자 ID, FK, NN, exchange:user = N:1
+    private int orderDetailId;  // 주문 상세 ID, FK, exchange:order_detail = NN, 1:1
+    private String productId;  // 상품 ID, FK, NN, exchange:product = 1:1
+    private String exMst;  // 교환처리상태_MST, FK, NN, exchange:common_code = N:1
+    private String exDet;  // 교환처리상태_DET, FK, NN, exchange:common_code = N:1
+    private String rfrMst;  // 환불/교환 사유_MST, FK, NN, exchange:common_code = N:1
+    private String rfrDet;  // 환불/교환 사유_DET, FK, NN, exchange:common_code = N:1
     private String exchaReason;  // 교환 사유, NN
     private int exchaCount;  // 교환 수량, NN
     private Date exchaDate;  // 교환일, NN
--- gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
@@ -7,12 +7,12 @@ import java.util.Date;
 @Data
 public class RefundDto {
     private int refundId;  // 환불 ID, PK, NN
-    private int userId;  // 사용자 ID, FK, NN, N:1
-    private int orderDetailId;  // 주문 상세 ID, FK, NN, 1:1
-    private String rfMst;  // 환불처리상태_MST, FK, NN, N:1
-    private String rfDet;  // 환불처리상태_DET, FK, NN, N:1
-    private String rfrMst;  // 환불/교환 사유_MST, FK, NN, N:1
-    private String rfrDet;  // 환불/교환 사유_DET, FK, NN, N:1
+    private int userId;  // 사용자 ID, FK, NN, refund:user = N:1
+    private int orderDetailId;  // 주문 상세 ID, FK, NN, refund:order_detail = 1:1
+    private String rfMst;  // 환불처리상태_MST, FK, NN, refund:common_code = N:1
+    private String rfDet;  // 환불처리상태_DET, FK, NN, refund:common_code = N:1
+    private String rfrMst;  // 환불/교환 사유_MST, FK, NN, refund:common_code = N:1
+    private String rfrDet;  // 환불/교환 사유_DET, FK, NN, refund:common_code = N:1
     private String reason;  // 환불 사유, NN
     private int count;  // 환불 수량, NN
     private Date date;  // 환불 일자, NN
