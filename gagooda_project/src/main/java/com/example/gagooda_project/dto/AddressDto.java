a9bb1209fe03069cfc9a901e669a3ee8be05f750
khs <kkhs00224@gmail.com>
2023-02-04 09:16:20 +0900

    AddressDto, DeliveryDto 20230204 update

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/AddressDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/AddressDto.java
@@ -5,13 +5,13 @@ import lombok.Data;
 
 @Data
 public class AddressDto {
-    private int addressId;  //배송지 ID
-    private int userId; // 사용자 ID
-    private String addressName; // 배송지 이름
-    private boolean addressDefault;  // 기본 배송지 true:기본배송지O, false:기본배송지X
-    private String address1; // 우편번호
-    private String address2; // 주소
-    private String address3; // 상세주소
-    private String receiverName; // 수령자 이름
-    private String receiverPhone; // 수령자 전화번호(12자 제한)
+    private int addressId;  // address_id  PK  NN  배송ID
+    private int userId; // user_id  FK  NN  (USER : ADDRESS) 1 : N  유저ID
+    private String addressName; // address_name NULL  배송지 이름
+    private boolean addressDefault;  // address_default  NN  true(1):기본 배송지, false(0):그냥 배송지
+    private String post_code; // post_code  NN  우편번호
+    private String address; // address NN  주소
+    private String addressDetail; // address_detail  NN 상세주소
+    private String receiverName; // receiver_name  NN  수령자 이름
+    private String receiverPhone; //  receiver_phone  NN  수령자 전화번호(12자 제한)
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/dto/DeliveryDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/DeliveryDto.java
@@ -7,13 +7,12 @@ import java.util.Date;
 // OrderDto 써야함
 @Data
 public class DeliveryDto {
-    private int deliveryId; // 배송 ID
-    private int orderId; // 주문 ID(1:1)
-    private String dMst; // 배송 상태 마스터 코드(공통 코드)
-    private String dDet; // 배송 상태 상세 코드(공통 코드)
-    private int invoice; // 운송장 번호 11자리
-    private Date startDate; // 배송 시작일
-    private Date endDate; // 배송 완료일
-    private String request; // 배송 요청사항
+    private int deliveryId; // delivery_id  PK  NN  배송ID
+    private int orderId; // order_id  FK  NN  (주문:배송) 1:1  주문ID
+    private String dDet; // d_det  NN  배송 상태_det(공통 코드)  DEFAULT 'd0'
+    private int invoice; // invoice  운송장 번호(11자리)
+    private Date startDate; // start_date 배송 시작일
+    private Date endDate; // end_date 배송 완료일
+    private String request; // request 배송 요청사항
 
 }
