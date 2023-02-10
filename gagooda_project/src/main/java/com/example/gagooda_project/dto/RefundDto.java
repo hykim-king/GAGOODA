b29c693b429abc18f6d222034ef9fc96ae7d1ce2
yangrangrang <gkssk2309@gmail.com>
2023-02-10 17:29:08 +0900

    refundDto,reviewDto최종수정

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
@@ -6,20 +6,21 @@ import java.util.Date;
 
 @Data
 public class RefundDto {
-    private int refundId;           // refund_id, PK, NN
-    private int userId;             // user_id, FK, NN, ( REFUND : USER ) N:1
-    private int orderId;            // order_id, FK, NN ( REFUND : ORDER ) 1:1
-    private int orderDetailId;      // order_detail_id, FK, NN, ( REFUND : ORDER_DTEAIL ) 1:1
-    private int addressId;          // address_id, FK, NN ( REFUND : ADDRESS ) 1:1
-    private int cancelAmount;       // cancel_amount
-    private String productId;       // product_id, FK, NN, ( REFUND : PRODUCT ) 1:N
-    private String optionId;        // option_id, FK, NN ( PRODUCT : OPTION) 1:N
-    private String rfrDet;          // rfr_det, FK, NN, ( REFUND : COMMON_CODE ) N:1
-    private String reason;          // reason, NN
-    private String comment;         // comment, NN
-    private Date date;              // date, NN
-    private Date modDate;           // mod_date, NULL
-    private String imgCode;         // img_code, NULL
+    private int refundId;           // 환불 ID, refund_id, PK, NN
+    private int userId;             // 사용자 ID, user_id, FK, NN, ( REFUND : USER ) N:1
+    private int orderId;            // 주문 ID, order_id, FK, NN ( REFUND : ORDER ) 1:1
+    private int orderDetailId;      // 주문상세 ID, order_detail_id, FK, NN, ( REFUND : ORDER_DTEAIL ) 1:1
+    private int addressId;          // 배송지 ID, address_id, FK, NN ( REFUND : ADDRESS ) 1:1
+    private String productId;       // 상품 ID, product_id, FK, NN, ( REFUND : PRODUCT ) 1:N
+    private String optionId;        // 옵션상품 ID, option_id, FK, NN ( PRODUCT : OPTION) 1:N
+    private int cancelAmount;       // 취소 요청 금액, cancel_amount
+    private String reason;          // 환불사유, reason, NN
+    private String comment;         // 환불 답변 사유, comment, NN
+    private Date regdate;           // 환불 요청 일자, reg_date, NN
+    private Date modDate;           // 환불 상태 변경 일자, mod_date, NULL
+    private String imgCode;         // 이미지 코드, img_code, NULL
+    private String rfDet;           // 환불처리상태_DET, FK, NN, ( REFUND : COMMON_CODE ) N:1
+    private String rfrDet;          // 환불/ 교환사유_DET, rfr_det, FK, NN, ( REFUND : COMMON_CODE ) N:1
 
 
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/dto/ReviewDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/ReviewDto.java
@@ -6,12 +6,12 @@ import java.util.Date;
 
 @Data
 public class ReviewDto {
-    private int reviewId;       // review_id, PK, NN
-    private int userId;         // user_id, FK, NN, ( REVIEW : USER ) 1:1
-    private int productId;      // product_id, FK , NN, ( REVIEW : PRODUCT ) 1:N
-    private String optionId;    // option_id, FK, NN, ( PRODUCT : OPTION ) 1:N
-    private String content;     // content, NN
-    private String imgCode;     // img_code, NULL
-    private double rate;        // rate, NN
-    private Date regDate;       // reg_date, NN
+    private int reviewId;       // 상품 후기ID, review_id, PK, NN
+    private int userId;         // 사용자 ID, user_id, FK, NN, ( REVIEW : USER ) 1:1
+    private int productId;      // 상품 ID, product_id, FK , NN, ( REVIEW : PRODUCT ) 1:N
+    private String optionId;    // 옵션상품 ID, option_id, FK, NN, ( PRODUCT : OPTION ) 1:N
+    private String content;     // 후기 내용, content, NULL
+    private String imgCode;     // 이미지 코드, img_code, NULL
+    private double rate;        // 평점, rate, NN
+    private Date regDate;       // 등록일, reg_date, NN
 }
