2122eac1c5293d7eec038ac947d21168283aeb65
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-02-10 16:41:22 +0900

    OrderDetailDto와 UserDto 수정

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/OrderDetailDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/OrderDetailDto.java
@@ -3,12 +3,11 @@ import lombok.Data;
 
 @Data
 public class OrderDetailDto {
-    private int orderDetailId;  // 주문 상세 아이디
-    private String optionName;  // 옵션 상품명
-    private int count;          // 상품 수량
-    private int price;          // 상품 총 값
-    private int orderId;        // 주문 아이디
-    private String productId;   // 상품 아이디
-    private String odMst;       // 주문 상태_mst (공통코드)
-    private String odDet;       // 주문 상태_det (공통코드)
+    private int orderDetailId;      // order_detail_id PK NN 주문 상세 아이디
+    private int orderID;            // order_id PK & FK (ORDER : ORDER_DETAIL) 1:N NN 주문 아이디
+    private String productId;       // product_id FK (ORDER_DETAIL:PRODUCT) 1:1 NN 상품 아이디
+    private String optionId;        // option_id FK (ORDER_DETAIL:OPTION_PRODUCT) 1:1 NN 상품 아이디
+    private int count;              // count NN 상품 수량
+    private int totalPrice;         // total_price NN 상품별 총 값
+
 }
