0837c5fc311068276ed86b9cc2b3c400536afcf2
yangrangrang <gkssk2309@gmail.com>
2023-02-10 16:59:27 +0900

    refundDto,ReviewDto설명 수정

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/RefundDto.java
@@ -7,14 +7,14 @@ import java.util.Date;
 @Data
 public class RefundDto {
     private int refundId;           // refund_id, PK, NN
-    private int userId;             // user_id, FK, NN, refund:user = N:1
-    private int orderId;            // order_id, FK, NN 1:1
-    private int orderDetailId;      // order_detail_id, FK, NN, refund:order_detail = 1:1
-    private int addressId;          // address_id, FK, NN 1:1
+    private int userId;             // user_id, FK, NN, ( REFUND : USER ) N:1
+    private int orderId;            // order_id, FK, NN ( REFUND : ORDER ) 1:1
+    private int orderDetailId;      // order_detail_id, FK, NN, ( REFUND : ORDER_DTEAIL ) 1:1
+    private int addressId;          // address_id, FK, NN ( REFUND : ADDRESS ) 1:1
     private int cancelAmount;       // cancel_amount
-    private String productId;       // product_id, FK, NN, N:1
-    private String optionId;        // option_id, FK, NN N:1
-    private String rfrDet;          // rfr_det, FK, NN, refund:common_code = N:1
+    private String productId;       // product_id, FK, NN, ( REFUND : PRODUCT ) 1:N
+    private String optionId;        // option_id, FK, NN ( PRODUCT : OPTION) 1:N
+    private String rfrDet;          // rfr_det, FK, NN, ( REFUND : COMMON_CODE ) N:1
     private String reason;          // reason, NN
     private String comment;         // comment, NN
     private Date date;              // date, NN
--- gagooda_project/src/main/java/com/example/gagooda_project/dto/ReviewDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/ReviewDto.java
@@ -7,9 +7,9 @@ import java.util.Date;
 @Data
 public class ReviewDto {
     private int reviewId;       // review_id, PK, NN
-    private int userId;         // user_id, FK, NN, 1:1
-    private int productId;      // product_id, FK , NN, N:1
-    private String optionId;    // option_id, FK, NN, N:1
+    private int userId;         // user_id, FK, NN, ( REVIEW : USER ) 1:1
+    private int productId;      // product_id, FK , NN, ( REVIEW : PRODUCT ) 1:N
+    private String optionId;    // option_id, FK, NN, ( PRODUCT : OPTION ) 1:N
     private String content;     // content, NN
     private String imgCode;     // img_code, NULL
     private double rate;        // rate, NN
