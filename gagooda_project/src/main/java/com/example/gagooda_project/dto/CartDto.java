5f2732dbd1ac365313bede090adbe6a22e05286d
cklee90 <cklee90@gmail.com>
2023-02-10 16:16:55 +0900

    dto작업전 커밋

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/CartDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/CartDto.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class CartDto {
     private int cart_id;        // 장바구니 ID
-    private int option_id;      // 옵션상품 ID
-    private int cart_cnt;       // 장바구니 상품 갯수
+    private String option_id;      // 옵션상품 ID
+    private String
+    private int count;       // 장바구니 상품 갯수
     private int user_id;        // 사용자 ID
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/dto/OrderDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/OrderDto.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 @Data
 public class OrderDto {
-    private int orderId;    // 주문 아이디  PK
-    private Date date;      // 주문일자     default(current_time stamp
-    private int userId;  // 회원 아이디
-    private int addressId;   // 배송지 아이디
-    private List<OrderDetailDto> orderDetailList;   // 1:N 주문 상세 테이블 연결
+    private int orderId;    // order_id 주문 아이디  PK  NN
+    private Date date;      // date 주문일자   default(current_timestamp) NN
+    private int userId;  // user_id 회원 아이디   NN
+    private int addressId;   // address_id 배송지 아이디   NN
+    /*private List<OrderDetailDto> orderDetailList; */  // 1:N 주문 상세 테이블 연결
 
 }
