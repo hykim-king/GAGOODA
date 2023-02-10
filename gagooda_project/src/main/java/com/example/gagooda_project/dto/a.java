83e94b311d2366419f55337a9167fbede38ff9a8
유한비 <119904360+hanbbi@users.noreply.github.com>
2023-02-10 17:27:40 +0900

    ProductDto, OptionProductDto 수정

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/a.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/a.java
@@ -4,28 +4,28 @@ import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 @Data
 class OptionProductDto {
-    private int oprionId; //옵션상품 ID
-    private String productId; //상품 ID
-    private int addition; //추가 상춤 여부 플래그
-    private String name; //옵션 상품 이름
-    private int price; //상품 가격
-    private int stock; //총 수량
+    private String optionId; // 옵션상품 ID option_id PK NN
+    private String productId; // 상품 ID product_id FK NN (상품:옵션상품 = 1:N)
+    private String name; // 옵션 상품 이름 NN
+    private int price; // 상품 가격 NN
+    private int stock; // 총 수량 NN
     private List<OptionProductDto> refundList;
 }
 @Data
 class CommonCodeDto {
-    private String mstCode; //마스터 코드
-    private String detCode; //상세코드
-    private String mstName; //마스터코드명
-    private String detName; //상세코드명
-    private int seq; //순서
-    private int useYn; //사용여부
-    private Date regDt; //등록일
-    private String regId; //등록자
-    private Date modDt; //수정일
-    private String modId; //수정자
+    private String mstCode; // 마스터 코드 PK NN
+    private String detCode; // 상세코드 PK NN
+    private String mstName; // 마스터코드명 NN
+    private String detName; // 상세코드명 NN
+    private int seq; // 순서 NULL
+    private Boolean userYn; // 사용여부 NULL
+    private Date regDt; // 등록일 NN
+    private String regId; // 등록자 NN
+    private Date modDt; // 수정일 NN
+    private String modId; // 수정자 NN
     private List<CommonCodeDto> refundList;
 }
