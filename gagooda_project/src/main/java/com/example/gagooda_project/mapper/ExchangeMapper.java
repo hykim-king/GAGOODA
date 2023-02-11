15fe1e7f16fc112b7f040b638318a6f23b523f8e
Wapl960212 <leo_960212@naver.com>
2023-02-11 14:38:11 +0900

    0211 xml

--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/ExchangeMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/ExchangeMapper.java
@@ -1,7 +1,20 @@
 package com.example.gagooda_project.mapper;
 
+import com.example.gagooda_project.dto.ExchangeDto;
+import com.example.gagooda_project.dto.RefundDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Date;
+import java.util.List;
 
 @Mapper
 public interface ExchangeMapper {
+    List<ExchangeDto> pageByUserAndDate(int id, Date curentDate);
+    List<ExchangeDto> pageAll();
+    int insertOne(RefundDto refund);
+    RefundDto findById(int id);
+    int updateOne(RefundDto refund);
+
+
+
 }
