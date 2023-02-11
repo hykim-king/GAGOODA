8c066037c1904352892afdb5b903a3ee1e7f3b8a
whileHoon <while@daum.net>
2023-02-11 15:05:40 +0900

    commit

--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/PInquiryCommentMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/PInquiryCommentMapper.java
@@ -1,7 +1,12 @@
 package com.example.gagooda_project.mapper;
 
+import com.example.gagooda_project.dto.PInquiryCommentDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PInquiryCommentMapper {
+    int findByInquiryId(int pInquiryId);
+    int insertOne(PInquiryCommentDto pInquiryComment);
+    int updateOne(PInquiryCommentDto pInquiryComment);
+    int deleteOne(int pInquiryId);
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/ReviewMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/ReviewMapper.java
@@ -4,4 +4,5 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ReviewMapper {
+
 }
