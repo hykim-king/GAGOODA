973d4bca6e5ea7a1690f95b9e7b3bf33c4e50410
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-02-11 15:08:23 +0900

    주문 mapper

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/OrderDetailDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/OrderDetailDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 @Data
 public class OrderDetailDto {
     private int orderDetailId;      // order_detail_id PK NN 주문 상세 아이디
-    private int orderID;            // order_id PK & FK (ORDER : ORDER_DETAIL) 1:N NN 주문 아이디
+    private int orderId;            // order_id PK & FK (ORDER : ORDER_DETAIL) 1:N NN 주문 아이디
     private String productCode;       // product_code FK (ORDER_DETAIL:PRODUCT) 1:1 NN 상품 아이디
     private String optionCode;        // option_code FK (ORDER_DETAIL:OPTION_PRODUCT) 1:1 NN 상품 아이디
     private int count;              // count NN 상품 수량 DEFAULT 0
