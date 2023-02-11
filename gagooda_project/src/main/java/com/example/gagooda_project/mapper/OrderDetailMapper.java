3b3c007c28dedb3e8306c5bf8f5960f1e3eec165
유한비 <119904360+hanbbi@users.noreply.github.com>
2023-02-11 15:07:46 +0900

    Address, OrderDetail Mapper 작성

--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/OrderDetailMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/OrderDetailMapper.java
@@ -1,7 +1,14 @@
 package com.example.gagooda_project.mapper;
 
+import com.example.gagooda_project.dto.OrderDetailDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface OrderDetailMapper {
+    List<OrderDetailDto> findByOrderId(int id);
+    OrderDetailDto findByOrderDetailId(int id);
+    int countByOrderId(int id);
+    int insertOne(OrderDetailDto dto);
 }
