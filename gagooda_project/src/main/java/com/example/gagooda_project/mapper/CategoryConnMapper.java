fc407194c1ca3ecdc2b64bbad8e63e7198dc3485
hamin081234 <hamin081234@gmail.com>
2023-02-11 15:03:30 +0900

    commit

--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/CategoryConnMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/CategoryConnMapper.java
@@ -1,7 +1,14 @@
 package com.example.gagooda_project.mapper;
 
+import com.example.gagooda_project.dto.CategoryConnDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CategoryConnMapper {
+    List<CategoryConnDto> listByCategoryId(int categoryId);
+    List<CategoryConnDto> listByProductCode(String productCode);
+    int insertOne(CategoryConnDto categoryConn);
+    int deleteByProductCode(String productCode);
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/ProductMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/ProductMapper.java
@@ -1,7 +1,16 @@
 package com.example.gagooda_project.mapper;
 
+import com.example.gagooda_project.dto.ProductDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ProductMapper {
+    ProductDto findById(String productCode);
+    int insertOne(ProductDto product);
+    int updateOne(ProductDto product);
+    List<ProductDto> listAll();
+    int deleteById(String productCode);
+//    List<ProductDto> pageAll();
 }
