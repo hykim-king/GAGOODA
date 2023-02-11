217b49e926ac1aebdb5a2cc7c85ce5ab067eb179
koodoyoon <kooodoyoon@gmail.com>
2023-02-11 15:03:31 +0900

    imgMapper userMapper 작성

--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/UserMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/UserMapper.java
@@ -1,7 +1,25 @@
 package com.example.gagooda_project.mapper;
 
+import com.example.gagooda_project.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
+    int insertOne(UserDto dto);
+
+    int deleteById(int userId);
+
+    int updateOne(UserDto dto);
+
+    UserDto findByEmailAndPw(String email, String pw);
+
+    UserDto findByEmailAndName(String email, String name);
+
+    int updatePw(String pw);
+
+    List<UserDto> listAll();
+
+//    List<UserDto> pageAll();  // PagingDto paging 필요
 }
