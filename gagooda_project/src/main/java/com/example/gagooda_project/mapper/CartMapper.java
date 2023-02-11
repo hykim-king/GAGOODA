61b9cae74565bd991023c08ab180ddb570237adb
cklee90 <cklee90@gmail.com>
2023-02-11 15:07:40 +0900

    mapper update 완료

--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/CartMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/CartMapper.java
@@ -1,7 +1,16 @@
 package com.example.gagooda_project.mapper;
 
+import com.example.gagooda_project.dto.CartDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CartMapper {
+    int insertOne(CartDto dto);
+    int deleteById(int id);  // cart_id
+    int deleteAll(int id);   // user_id
+    int countByUserId(int id);
+    List<CartDto> listByUserId(int id);
+
 }
