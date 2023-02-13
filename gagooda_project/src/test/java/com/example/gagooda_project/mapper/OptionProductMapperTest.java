839140f643d20406ed5a53ab6b7cac5ec5a982f9
whileHoon <while@daum.net>
2023-02-13 17:58:47 +0900

    commit

---
+++ gagooda_project/src/test/java/com/example/gagooda_project/mapper/OptionProductMapperTest.java
@@ -0,0 +1,25 @@
+package com.example.gagooda_project.mapper;
+
+import com.example.gagooda_project.dto.OptionProductDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OptionProductMapperTest {
+    @Test
+    void listByProductCode() {
+    }
+
+    @Test
+    void insertOne() {
+    }
+
+    @Test
+    void deleteByProductCode() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
+
