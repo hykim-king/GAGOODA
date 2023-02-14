6057b844ff455af415717ec4b408099544be8c28
유한비 <119904360+hanbbi@users.noreply.github.com>
2023-02-14 15:13:31 +0900

    2/13 mappertest commit

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/DeliveryDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/DeliveryDto.java
@@ -7,11 +7,11 @@ import java.util.Date;
 // OrderDto 써야함
 @Data
 public class DeliveryDto {
-    private int delivery_id; // 배송 ID PK NN AUTO_INCREMENT order:delivery_id = 1:1
-    private int order_id; // 주문 ID(1:1) FK NN
+    private int deliveryId; // 배송 ID PK NN AUTO_INCREMENT order:delivery_id = 1:1
+    private String orderId; // 주문 ID(1:1) FK NN
     private int invoice; // 운송장 번호 11자리
-    private Date start_date; // 배송 시작일
-    private Date end_date; // 배송 완료일
+    private Date startDate; // 배송 시작일
+    private Date endDate; // 배송 완료일
     private String request; // 배송 요청사항
-    private String d_det; // 배송 상태 상세 코드(공통 코드) NN
+    private String dDet; // 배송 상태 상세 코드(공통 코드) NN
 }
---
+++ gagooda_project/src/test/java/com/example/gagooda_project/mapper/DeliveryMapperTest.java
@@ -0,0 +1,43 @@
+package com.example.gagooda_project.mapper;
+
+import com.example.gagooda_project.dto.DeliveryDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DeliveryMapperTest {
+
+    @Autowired
+    DeliveryMapper deliveryMapper;
+
+    @Test
+    void insertOne() {
+        DeliveryDto delivery = new DeliveryDto();
+        delivery.setInvoice(1357);
+        delivery.setRequest("안전배송");
+        delivery.setDDet("d0");
+        delivery.setOrderId("1");
+        deliveryMapper.insertOne(delivery);
+    }
+
+    @Test
+    void updateOne() {
+        DeliveryDto delivery = new DeliveryDto();
+        delivery.setDeliveryId(1);
+        delivery.setInvoice(1234);
+        delivery.setRequest("빠른배송");
+        delivery.setDDet("d0");
+        delivery.setOrderId("1");
+        deliveryMapper.updateOne(delivery);
+        DeliveryDto check = deliveryMapper.findById(1);
+        System.out.println(check);
+    }
+
+    @Test
+    void findById() {
+        DeliveryDto delivery = deliveryMapper.findById(1);
+    }
+}
