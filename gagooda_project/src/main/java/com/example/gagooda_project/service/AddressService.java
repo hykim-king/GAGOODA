0b0c21efc84ccec1e3d3feccbabfdf53509c8f2f
cklee90 <cklee90@gmail.com>
2023-02-16 09:09:58 +0900

    어드레스 컨트롤러 수정/삭제 기능 완료

--- gagooda_project/src/main/java/com/example/gagooda_project/service/AddressService.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/service/AddressService.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public interface AddressService {
     int register(AddressDto address); //했고
-    AddressDto selectOne(int addressId);
-    int modifyOne(AddressDto address);
-    AddressDto defaultAddress(int addressId);
+    AddressDto selectOne(int addressId); //했고
+    int modifyOne(AddressDto address); //했고
+    AddressDto defaultAddress(int userId);
     List<AddressDto> addressList(int userId); // 했고 default address 는 빠진 주소 목록
-    int modifyDefault(int addressId);
-    int removeOne(int addressId);
+    int modifyDefault(int userId);
+    int removeOne(int addressId); // 했고
 
 
 }
