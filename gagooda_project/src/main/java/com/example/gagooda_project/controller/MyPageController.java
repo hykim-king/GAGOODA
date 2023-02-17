e3599a8b9a18351c9401fabdc3176456d9d5c1a5
Wapl960212 <leo_960212@naver.com>
2023-02-17 10:41:14 +0900

    0217

--- gagooda_project/src/main/java/com/example/gagooda_project/controller/MyPageController.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/controller/MyPageController.java
@@ -1,11 +1,11 @@
 package com.example.gagooda_project.controller;
 
-import com.example.gagooda_project.service.MyPageService;
+import com.example.gagooda_project.service.MyPageServiceImp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/myPageController")
 public class MyPageController {
-    MyPageService myPageService;
+    MyPageServiceImp myPageService;
 }
---
+++ gagooda_project/src/main/java/com/example/gagooda_project/service/MyPageServiceImp.java
@@ -0,0 +1,9 @@
+package com.example.gagooda_project.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyPageServiceImp implements MyPageService {
+
+}
