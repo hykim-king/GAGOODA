d24a1acaa207a6958a8bccef2670bd8c274971cb
cklee90 <cklee90@gmail.com>
2023-02-21 15:21:49 +0900

    현타 치료중

--- gagooda_project/src/main/java/com/example/gagooda_project/dto/DeliveryDto.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/dto/DeliveryDto.java
@@ -7,15 +7,18 @@ import java.util.Date;
 // OrderDto 써야함
 @Data
 public class DeliveryDto {
-    private int deliveryId; // 배송 ID PK NN AUTO_INCREMENT order:delivery_id = 1:1
-    private Integer invoice; // 운송장 번호 11자리
+    private String orderId; // 배송 ID PK NN AUTO_INCREMENT order:delivery_id = 1:1
+    private int userId;  // 유저 ID (1:N)  NN
+    private String userName;
+    private String userEmail;
+    private String userPhone;
+    private int invoice; // 운송장 번호 11자리
     private Date startDate; // 배송 시작일
     private Date endDate; // 배송 완료일
     private String request; // 배송 요청사항
     private String dDet; // 배송 상태 상세 코드(공통 코드) NN FK (1:1)
-    private String orderId; // 주문 ID(1:1) FK NN
-    private String userId;  // 유저 ID (1:N) FK NN
     private OrderDto orderDto;
+
 
 
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/mapper/DeliveryMapper.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/mapper/DeliveryMapper.java
@@ -8,11 +8,11 @@ import java.util.Map;
 
 @Mapper
 public interface DeliveryMapper {
-    int insertOne(DeliveryDto dto);
+
     int updateOne(DeliveryDto dto);
-    DeliveryDto findById(int id);
     DeliveryDto findByOrderId(String orderId);
-    List<DeliveryDto> listAll(Map<String, String> searchFilter);
-    List<DeliveryDto> pageByUserIdAndDate(int userId, int period, String startDate, String endDate, String detCode);
+    List<DeliveryDto> listAll(Map<String, String> searchFilter); // 관리자 입장 다 뽑아보기
+    List<DeliveryDto> pageByUserIdAndDate(int userId, int period, String startDate, String endDate, String detCode); //유저입장에서 본인 아이디 해당 리스트 다 보기
+    int deleteOne(DeliveryDto dto);
 
 }
