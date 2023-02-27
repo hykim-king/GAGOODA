77aa2c4308bb4c108eab0135511138b02e573121
cklee90 <cklee90@gmail.com>
2023-02-27 17:48:09 +0900

    배송,주소부분 session msg 추가

--- gagooda_project/src/main/java/com/example/gagooda_project/controller/AddressController.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/controller/AddressController.java
@@ -3,6 +3,7 @@ package com.example.gagooda_project.controller;
 import com.example.gagooda_project.dto.AddressDto;
 import com.example.gagooda_project.dto.UserDto;
 import com.example.gagooda_project.service.AddressService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,26 @@ public class AddressController {
     }
 
     @GetMapping("/user_yes/mypage/register.do")
-    public String register(@SessionAttribute(required = true) UserDto loginUser) {
-        return "/address/register";
+    public String register(@SessionAttribute(required = true) UserDto loginUser,
+                           @SessionAttribute(required = false) String msg,
+                           HttpSession session,
+                           Model model) {
+        if (msg != null) {
+            session.removeAttribute("msg");
+            model.addAttribute("msg",msg);
+        }
+        try{
+            return "/address/register";
+        } catch(Exception e) {
+            session.setAttribute("msg","등록 중 오류가 발생하셨습니다.");
+            return "redirect:/user_yes/mypage/register.do";
+        }
     }
 
     @PostMapping("/user_yes/mypage/register.do")
     public String register(AddressDto address,
-                           @SessionAttribute(required = true) UserDto loginUser) {
+                           @SessionAttribute(required = true) UserDto loginUser,
+                           HttpSession session) {
         int register = 0;
         try {
             if(addressService.defaultAddress(address.getUserId())==null) {
@@ -44,29 +58,53 @@ public class AddressController {
         if (register > 0) {
             return "redirect:/address/user_yes/mypage/list.do";
         } else {
+            session.setAttribute("msg", "등록 중 오류가 발생했습니다.");
             return "redirect:/address/user_yes/mypage/register.do";
         }
     }
 
     @GetMapping("/user_yes/mypage/list.do")
     public String addressList(Model model,
-                              @SessionAttribute(required = true) UserDto loginUser) {
-        System.out.println(loginUser);
-        List<AddressDto> alist = addressService.addressList(loginUser.getUserId());
-        model.addAttribute("alist",alist);
-        System.out.println(alist);
-        return "address/list";
+                              @SessionAttribute(required = true) UserDto loginUser,
+                              @SessionAttribute(required = false) String msg,
+                              HttpSession session) {
+//        System.out.println(loginUser);
+        if(msg!=null) {
+            session.removeAttribute("msg");
+            model.addAttribute("msg",msg);
+        }
+        try{
+            List<AddressDto> alist = addressService.addressList(loginUser.getUserId());
+            model.addAttribute("alist",alist);
+            return "address/list";
+        }catch (Exception e){
+            e.printStackTrace();
+            session.setAttribute("msg", "데이터를 찾을 수 없습니다.");
+            return "redirect:/";
+        }
     }
 
 
 
     @GetMapping("/user_yes/mypage/modify.do")
     public String modifyOne(@SessionAttribute(required = true) UserDto loginUser,
+                            @SessionAttribute(required = false) String msg,
+                            HttpSession session,
                             Model model,
                             @RequestParam (name= "addressId") int addressId) {
-        AddressDto address = addressService.selectOne(addressId);
-        model.addAttribute("address",address);
-        return "address/modify";
+        if (msg!=null) {
+            model.addAttribute("msg",msg);
+            session.removeAttribute("msg");
+        }
+        try {
+            AddressDto address = addressService.selectOne(addressId);
+            model.addAttribute("address",address);
+            return "address/modify";
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.setAttribute("msg","수정 폼을 불러올 수 없습니다.");
+            return "redirect:/";
+        }
     }
 
     @GetMapping("/user_yes/mypage/dismissDefault.do")
@@ -85,7 +123,8 @@ public class AddressController {
 
     @PostMapping("/user_yes/mypage/modify.do")
     public String modifyOne(AddressDto address,
-                            @SessionAttribute(required = true) UserDto loginUser) {
+                            @SessionAttribute(required = true) UserDto loginUser,
+                            HttpSession session) {
         int modify=0;
         if(loginUser.getUserId() == address.getUserId()){
             try {
@@ -107,14 +146,22 @@ public class AddressController {
         if(modify>0) {
             return "redirect:/address/user_yes/mypage/list.do";
         } else {
+            session.setAttribute("msg","수정 중 오류가 발생했습니다.");
             return "redirect:/address/user_yes/mypage/modify.do?addressId="+address.getAddressId();
         }
     }
 
     @GetMapping("/user_yes/mypage/delete.do")
     public String removeOne(@RequestParam(name = "address_id") int addressId,
-                            @SessionAttribute(required = true) UserDto loginUser) {
+                            @SessionAttribute(required = true) UserDto loginUser,
+                            @SessionAttribute(required = false) String msg,
+                            HttpSession session,
+                            Model model) {
         int remove = 0;
+        if(msg!=null) {
+            session.removeAttribute("msg");
+            model.addAttribute("msg",msg);
+        }
         AddressDto address = addressService.selectOne(addressId);
         if (loginUser.getUserId() == address.getUserId()) {
             try {
@@ -126,8 +173,8 @@ public class AddressController {
         if(remove>0) {
             return "redirect:list.do";
         } else {
+            session.setAttribute("msg","상품 삭제에 실패했습니다.");
             return "redirect:modify.do?addressId="+address.getAddressId();
         }
-
     }
 }
--- gagooda_project/src/main/java/com/example/gagooda_project/controller/DeliveryController.java
+++ gagooda_project/src/main/java/com/example/gagooda_project/controller/DeliveryController.java
@@ -8,6 +8,7 @@ import com.example.gagooda_project.service.DeliveryService;
 import com.example.gagooda_project.service.OrderService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -31,9 +32,15 @@ public class DeliveryController {
     }
     @GetMapping("user_yes/mypage/{orderId}/detail.do")
     public String detail(@SessionAttribute UserDto loginUser,
+                         @SessionAttribute(required = false) String msg,
                          @PathVariable String orderId,
+                         HttpSession session,
                          Model model) {
         DeliveryDto delivery = deliveryService.selectByOrderId(orderId);
+        if (msg!=null) {
+            session.removeAttribute("msg");
+            model.addAttribute("msg",msg);
+        }
         if(loginUser.getUserId() == delivery.getUserId()) {
             try{
                 List<CommonCodeDto> allDList = deliveryService.showDetCodeList("d");
@@ -41,6 +48,7 @@ public class DeliveryController {
                 model.addAttribute("dCodeList",allDList);
                 return "delivery/detail";
             } catch(Exception e) {
+                session.setAttribute("msg","데이터를 가져오지 못 했습니다.");
                 e.printStackTrace();
             }
         }
@@ -48,6 +56,8 @@ public class DeliveryController {
     }
     @GetMapping("admin/list.do")
     public String showDeliveryList(@SessionAttribute UserDto loginUser,
+                                   @SessionAttribute(required = false) String msg,
+                                   HttpSession session,
                                    PagingDto paging,
                                    HttpServletRequest req,
                                    @RequestParam(name = "dDet", required = false, defaultValue = "") String dDet,
@@ -57,6 +67,10 @@ public class DeliveryController {
                                    @RequestParam(name = "startDate", required = false, defaultValue = "") String startDate,
                                    @RequestParam(name = "endDate", required = false, defaultValue = "") String endDate,
                                    Model model) {
+        if(msg!=null) {
+            session.removeAttribute("msg");
+            model.addAttribute("msg",msg);
+        }
         try {
             if(loginUser.getGDet().equals("g1")) {
                 Map<String, Object> searchFilter = new HashMap<>();
@@ -73,6 +87,7 @@ public class DeliveryController {
                 model.addAttribute("deliveryCount",deliveryCount);
                 return "delivery/list";
             } else {
+                session.setAttribute("msg","배송 정보를 불러오는데에 실패했습니다.");
                 return "/index";
             }
         } catch (Exception e) {
@@ -83,21 +98,37 @@ public class DeliveryController {
 
     @GetMapping("admin/{orderId}/modify.do")
     public String modifyOne(@SessionAttribute UserDto loginUser,
+                            @SessionAttribute(required = false) String msg,
+                            HttpSession session,
                             Model model,
                             @PathVariable String orderId){
         DeliveryDto delivery = deliveryService.selectByOrderId(orderId);
-        model.addAttribute("delivery",delivery);
-        return "delivery/modify";
+        if(msg!=null) {
+            session.removeAttribute("msg");
+            model.addAttribute("msg",msg);
+        }
+        if(loginUser.getGDet().equals("g1")) {
+            try {
+                model.addAttribute("delivery",delivery);
+                return "delivery/modify";
+            } catch(Exception e) {
+                e.printStackTrace();
+                session.setAttribute("msg","데이터를 가져오는데 실패하였습니다.");
+                return "redirect:/";
+            }
+        } else {
+            session.setAttribute("msg","관리자만 배송정보를 수정할 수 있습니다.");
+            return "redirect:/";
+        }
     }
 
     @PostMapping("admin/modify.do")
     public String modifyOne(DeliveryDto delivery,
-                            @SessionAttribute UserDto loginUser) {
+                            @SessionAttribute UserDto loginUser,
+                            HttpSession session) {
         int modify = 0;
-        System.out.println("여기까진옴");
         try{
             if (loginUser.getGDet().equals("g1")) {
-                System.out.println("잘들어옴");
                 modify = deliveryService.modifyOne(delivery);
             }
         } catch (Exception e) {
@@ -106,19 +137,29 @@ public class DeliveryController {
         if(modify>0) {
             return "redirect:/delivery/admin/list.do";
         } else {
+            session.setAttribute("msg","배송 정보 수정 중 오류가 발생했습니다.");
             return "redirect:/delivery/admin"+delivery.getOrderId()+"/modify.do";
         }
     }
 
     @GetMapping("/admin/{orderId}/delete.do")
     public String removeOne(@PathVariable String orderId,
-                            @SessionAttribute UserDto loginUser) {
+                            @SessionAttribute UserDto loginUser,
+                            @SessionAttribute(required = false) String msg,
+                            HttpSession session,
+                            Model model) {
         int remove = 0;
+        if(msg != null) {
+            session.removeAttribute("msg");
+            model.addAttribute("msg",msg);
+        }
         try{
-            System.out.println("여기까진 오냐");
+//            System.out.println("여기까진 오냐");
             if(loginUser.getGDet().equals("g1")) {
                 System.out.println("여긴 왜 안와");
                 remove = deliveryService.removeOne(orderId);
+            } else {
+                session.setAttribute("msg","관리자만 삭제 할 수 있습니다.");
             }
         } catch(Exception e) {
             e.printStackTrace();
@@ -126,6 +167,7 @@ public class DeliveryController {
         if(remove>0) {
             return "redirect:/delivery/admin/list.do";
         } else {
+            session.setAttribute("msg","배송 정보를 삭제하는데 실패했습니다.");
             return "redirect:/delivery/admin/"+orderId+"/modify.do";
         }
     }
